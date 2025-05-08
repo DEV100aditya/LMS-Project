8377e5c6e1d9381f3437cbf7ed91922ee3fb0057
DEV100aditya <aditya123.kishan@gmail.com>
2025-05-09 02:24:26 +0530

    completed changes

--- server/controllers/educatorController.js
+++ server/controllers/educatorController.js
@@ -5,12 +5,13 @@ import { Purchase } from '../models/Purchase.js'
 
 // Update role to educator
 export const updateRoleToEducator = async (req, res)=>{
+
     try {
         const userId = req.auth.userId
 
         await clerkClient.users.updateUserMetadata(userId, {
             publicMetadata:{
-                role: 'educator',
+                role: 'educator'
             }
         })
 
